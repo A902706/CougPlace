375cfd77136cbeda9efff4284b2d24625738d251
A902706 <Sanchezandres5746@gmail.com>
2026-04-20 21:18:58 -0700

    Added a login page and sign up page.
    Login page requires username and password.
    Otherwise an option to create a new account appears. which requires name, email, age, and gender.
    Next: Send a code to verify user email.
    Forgot password page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/*Store current users*/
+let users = [];
+
+/* GET home page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/login');
+});
+
+/* GET login page */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/*GET signUp page */
+router.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
+router.post('/signup', function(req, res) {
+
+  const { firstName, lastName, email, gender, age } = req.body;
+
+  let errors = [];
+
+  // Age check
+  if (age < 18) {
+    errors.push("You must be 18 or older.");
+  }
+
+  // .edu email check
+  if (!email.endsWith('.edu')) {
+    errors.push("Email must contain \".edu\"");
+  }
+
+    // Store data so user doesnt have to reinput it.
+  users.push({
+    firstName,
+    lastName,
+    email,
+    gender,
+    age
+  });
+
+  if (errors.length > 0) {
+    return res.render('signup', { error: errors[0] });
+  }
+
+  // Print details.
+  console.log(users)
+
+  // Redirect to login page
+  return res.redirect('/login')
 });
 
 module.exports = router;
